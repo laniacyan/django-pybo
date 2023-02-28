14945410e39fd50f1a1cced14f9340f8dc1465c8
cyan <cyan_1202@naver.com>
2023-02-28 16:23:02 +0900

    vote 기능 완료

--- pybo/models.py
+++ pybo/models.py
@@ -4,22 +4,26 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Question(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question')
     subject = models.CharField('제목', max_length=200, 
                                help_text='질문의 제목을 한 줄로 작성하세요.')
     content = models.TextField('내용', help_text='질문의 내용을 상세히 작성하세요.')
     create_date = models.DateTimeField('생성일')
     modify_date = models.DateTimeField(null=True, blank=True)
 
+    voter = models.ManyToManyField(User, related_name='voter_question')
+
     def __str__(self):
         return f'{self.id} {self.subject}'
 
 class Answer(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_answer')
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField('답변 내용')
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
+
+    voter = models.ManyToManyField(User, related_name='voter_answer')
 
     def __str__(self):
         # return self.question
--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,5 +1,5 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404
 from ..models import Question
 
 
